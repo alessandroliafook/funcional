50a2cb1051c9f287b63b272b1029f3fc7d7b9aef
alessandroliafook <alessandro.liafook@gmail.com>
2019-04-01 14:49:17 -0300

    Atualizando Lista funções

--- funcoes-sintaxe.hs
+++ funcoes-sintaxe.hs
@@ -3,30 +3,31 @@
 - Ela deve ser indefinida quando o denominador for zero.
 -}
 divTuple (x, 0) = undefined
-divTuple (x, y) = x/y
+divTuple (x, y) = x / y
 
 {-
  - Calcula o somatorio entre dois numeros a e b (a < b). Procure usar alguma funcao pronta sobre listas. 
  - Ex: somatorio 0 1 = 1
  -     somatorio 1 3 = 6
 -}
-somatorio a b = sum [a..b]
+somatorio x y = sum [x..y]
 
 {-
  - Calcula o somatorio (recursivo) entre dois numeros a e b (a < b).
  - Ex: somatorio 0 1 = 1
  -     somatorio 1 3 = 6
 -}
-somatorioRec a b = if (a+1 == b) then a + b else a + somatorioRec (a+1) b
+somatorioRec a b | a == b = a
+                 | otherwise = b + somatorioRec a (b - 1)
 
 -- Defina a funcao que eleva um membro ao quadrado
-square x = x*x
+square x = x * x
 
 -- Soma os quadrados de dois numeros.
 sumSquares x y = square x + square y
 
 -- Defina uma funcao de alta ordem que aceita uma função (Int -> Int) e aplica a funcao a dois numeros
-higherOrderSum f a b = sum (map f [a,b])
+higherOrderSum f x y = f x + f y
 
 -- Defina a soma dos quadrados em termos de higherOrderSum
 hoSumSquares = higherOrderSum square
@@ -34,4 +35,15 @@ hoSumSquares = higherOrderSum square
 --Implemente a funcao mapFilter que primeiro aplica o map de uma funcao f a uma lista e depois aplica a funcao filter
 -- a lista resultante. Procure usar a composicao de funcoes
 mapFilter f p = (filter p).(map f)
---mapFilter f p xs = filter p (map f xs)
+
+-- tests
+
+main = do 
+          print (divTuple (4, 2))
+          print (somatorio 0 1)
+          print (somatorio 1 3)
+          print (somatorioRec 1 3)
+          print (square 3)
+          print (sumSquares 3 4)
+          print (higherOrderSum square 1 3)
+          print (mapFilter square even [2, 1, 2, 3, 4, 5])
--- introducao.hs
+++ introducao.hs
@@ -66,3 +66,16 @@ coprimo x y = (mdc x y) == 1
 - Calcula a conjectura de Goldbach, que diz que um numero par maior que 2 pode ser escrito como a soma de dois numeros primos. Ex: 28 = 5 + 23.
 -}
 goldbach x = [ (y,z)| y <- filter isPrime [1..(x-1)], z <- filter isPrime [1..(x-1)], y + z == x ]
+
+-- tests
+
+    main = do putStrLn "What is 5! ?"
+              x <- readLn
+              if x == fatorial 5
+                  then putStrLn "You're right!"
+                  else putStrLn "You're wrong!"
+              
+              y <- readLn
+              z <- readLn
+
+              print (goldbach y)
