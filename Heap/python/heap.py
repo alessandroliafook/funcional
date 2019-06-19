540fbb1804efaf92bf39d1b0ddf4387771fd20c9
alessandroliafook <alessandro.liafook@gmail.com>
2019-06-19 01:55:43 -0300

    Refatorando código pyhton e adicionando testes

--- Heap/python/heap.py
+++ Heap/python/heap.py
@@ -1,68 +1,61 @@
 class Heap:
 
-    def __init__(self, comparator=lambda a, b: return a > b): 
+    def __init__(self, comparator=lambda a, b: a > b): 
 
         self.array = []
         self.comparator = comparator
-        self.index = -1
 
-    def _parent(i):
+    def _parent(self, i):
         return (i - 1) / 2
     
-    def _left(i):
+    def _left(self, i):
         return (i * 2) + 1
     
-    def _right(i):
+    def _right(self, i):
         return (i * 2) + 2
     
-    def _swap(self, x, y):
-    
-        t = self.array[x]
-        self.array[x] = self.array[y]
-        self.array[y] = t
+    def _swap(self, x, y):    
+        self.array[x], self.array[y] = self.array[y], self.array[x]
 
     def isEmpty(self):
-        return index == -1
+        return len(self.array) == 0
     
     def toArray(self):
         return this.array.copy()
 
     def size(self):
-        return self.index + 1
+        return len(self.array)
 
     def getHeap(self):
-        return self.array 
+        return self.array.copy() 
     
     def clear(self):        
         self.array = []
-        self.index = -1
 
     def _heapify(self, position):
 
-        if position >= index:
+        if position >= self.size():
             return
         
         max = position
 
-        if(self._left(position) <= self.index and self.comparator(self.array[position], self.array[self._left(position)] == False)):
+        if(self._left(position) < self.size() and self.comparator(self.array[max], self.array[self._left(position)]) == False):
             max = self._left(position)
 
-        if(self._right(position) <= self.index and self.comparator(self.array[position], self.array[self._right(position)] == False)):
+        if(self._right(position) < self.size() and self.comparator(self.array[max], self.array[self._right(position)]) == False):
             max = self._right(position)
-        
+
         if max != position:
             self._swap(position, max)
             self._heapify(max)
 
     def insert(self, element):
 
-        if (len(self.array) >= 0 and type(element) != type(self.array[0])) or element == None:
+        if (len(self.array) > 0 and type(element) != type(self.array[0])) or element == None:
             return
 
         self.array.append(element)
-        self.index += 1
-
-        upHeap(self.index)
+        self.upHeap(self.size() - 1)
 
     def upHeap(self, position):
 
@@ -70,7 +63,7 @@ class Heap:
 
             parent = self._parent(position)
 
-            if self.comparator(self.array[position], self.array[parent]) == True):
+            if self.comparator(self.array[position], self.array[parent]) == True:
 
                 self._swap(position, parent)
                 self._heapify(position)
@@ -82,30 +75,28 @@ class Heap:
             return
         
         self.clear()
-
-        for el in array:
-            if el not None:
-                self.insert(el)
+        map(self.insert, array)
 
     def extractRootElement(self):
 
-        if index == -1:
+        if self.size() == 0:
             return None
         
         else:
             
             root = self.array[0]
 
-            self.array[0] = self.array[self.index]
-            self.array.pop(self.index)
-            self.index -= self.index
-            
-            self._heapify(0)
+            if self.size() > 1:
+                self.array[0] = self.array.pop(self.size() - 1)
+                self._heapify(0)
+
+            else:
+                self.clear()
 
             return root
 
     def rootElement(self):
-        return self.array[0] if index != -1 else None
+        return self.array[0] if self.size() > 0 else None
     
     def heapSort(self, array):
 
@@ -114,22 +105,35 @@ class Heap:
         if array == None:
             return out
         
-        comp = self.comparator
+        comp, l = self.comparator, self.array
 
-        self.comparator = lambda a, b: return a > b
-
+        self.comparator = lambda a, b: a < b
         self.clear()
 
-        for el in array:
-            self.insert(el)
+        map(self.insert, array)
+        
+        [out.append(self.extractRootElement()) for i in xrange(self.size())]
 
-        el = self.extractRootElement()
-
-        while el not None:
-            out.append(el)
-            el = self.extractRootElement()
-
-        self.clear
-        self.comparator = comp
+        self.clear()
+        self.comparator, self.array = comp, l
 
         return out
+
+
+if __name__ == "__main__":
+
+    h = Heap()
+    assert h.size() == 0
+    assert h.isEmpty() == True
+
+    h.buildHeap([82, 6, 99, 12, 34, 64, 58, 1])
+    assert h.size() == 8
+    assert h.isEmpty() == False
+    assert h.array == [99, 34, 82, 6, 12, 64, 58, 1]
+    assert h.extractRootElement() == 99
+    assert h.rootElement() == 82
+
+    h.clear()
+    assert h.size() == 0
+    assert h.isEmpty() == True
+    assert h.heapSort([82, 6, 99, 12, 34, 64, 58, 1]) == sorted([82, 6, 99, 12, 34, 64, 58, 1])
